ceabf1de67dcd6a0264bdb36ce144676b271312a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2012-12-27 16:15:17 +0100

    6-grid layout for landscape mobile phone

--- main.js
+++ main.js
@@ -2,117 +2,78 @@
     "use strict";
     function genStyles() {
         var width = 240;
-        var height = 280;
-        var unit = width / 6 | 0;
-        // margin .1 unit
-        // height .8 unit
-        var largeFont = unit * .6;
-        var mediumFont = largeFont * .75;
-        var smallFont = largeFont * .5;
-        console.log("unit", unit, unit * .8);
-        var inputMargin = unit * 0.1  | 0;
-        var result = {
-            w1: {
-                width: .8*unit - 2,
-                margin: .1 * unit - 1,
-                borderWidth: 1,
+        var height = 320;
+        var margin = (width / 40) & ~1;
+        var unit = ((width - 7 * margin)/6) | 0;
+        var margin0 = (width - 7 * margin - unit * 6) >> 1;
+        var smallFont = unit * .4;
+        function wn(n) {
+            return {
+                verticalAlign: "middle",
+                border: "none",
+                padding: 0,
+                marginLeft: margin,
+                marginRight: 0,
+                width: unit * n + margin * (n-1),
                 display: "inline-block",
+                boxShadow: "1px 1px 4px rgba(0,0,0,1)",
+            };
+        }
+        var result = {
+            line: {
+                marginTop: margin,
+                textAlign: "center",
+                height: unit,
             },
             page: {
+                verticalAlign: "middle",
+                overflow: "hidden",
                 lineHeight: "100%",
                 fontSize: smallFont,
                 fontFamily: "sans-serif",
                 border: "1px solid black",
-                margin: 0,
+                margin: unit/2,
                 padding: 0,
                 display: "inline-block",
                 width: width,
                 height: height,
+                background: "white",
             },
             header: {
                 position: "fixed",
-                height: unit, width: width,
-                boxShadow: "0 4px 4px rgba(0,0,0,.4)",
-                background: "rgba(255,255,255,.7)"
+                height: unit+margin, width: width,
+                background: "rgba(255,255,255,.7)",
             },
-            backIcon: {
-                float: "left",
-                fontSize: largeFont,
-                border: "solid black",
-                height: unit * 0.8 - 2,
-                borderRadius: unit / 6,
-            },
-            searchIcon: {
-                position: "absolute",
-                left: 5*unit,
-                fontSize: largeFont,
-                background: "#aff",
-                height: unit * 0.8 - 2,
-                border: "solid black",
-                borderRadius: unit / 6,
-            },
-            sb4: {
-                width: 3.7*unit - 2,
-                margin: .1 * unit - 1,
-                borderWidth: 1,
-                display: "inline-block",
-            },
-            searchBox: {
-                position: "absolute",
-                top: 0,
-                left: unit,
-                width: 3.9 * unit - 2,
-                lineHeight: "100%",
-                fontSize: (unit * .8  - 6)  / 2 | 0,
-                fontFamily: "sans-serif",
-                marginTop: .1*unit,
-                border: "1px solid black",
-                height: .8*unit - 5,
-                padding: 1,
-            },
-            searchLine: {
-                width: 4.7*unit - 2,
-                margin: .1 * unit - 1,
-                fontSize: mediumFont,
-                height: unit* 0.6 - 2,
-                border: "1px solid black",
-                padding: 0.1 * unit,
-            },
-            searchResult: {
-                display: "inline-block",
-                height: unit,
+            largeWidget: {
+                marginTop: margin,
+                height: (height - unit * 3 - margin * 6) >>1,
+                marginLeft: margin,
+                marginRight: margin,
                 overflow: "hidden",
-                border: "1px solid black",
-                margin: 0,
-                padding: 0,
+                boxShadow: "1px 1px 4px rgba(0,0,0,1)",
             },
             resultImg: {
                 float: "left",
-                height: unit,
+                height: 1.618 * unit,
                 width: unit,
+                marginRight: margin,
+                marginBottom: margin,
             },
-            headerPadding: {
-                height: unit
+            resultOrderButton: {
+                float: "right",
+                height: unit,
             },
-            biblogo: {
-                width: width,
-                paddingTop: .4*unit,
-                height: .6*unit, 
-                fontSize: mediumFont,
-                background: "#fee",
-            },
-            patronWidget: {
-                height: unit, width: width/2,
-                background: "#eef",
-            },
-            largeWidget: {
-                height: (height - 3*unit) / 2,
-                background: "#fbf",
+            searchResult: {
+                marginTop: margin,
+                height: 1.618 * unit,
                 overflow: "hidden",
             },
-        };
+            headerPadding: { height: unit+margin },
+            w1: wn(1), w2: wn(2), w3: wn(3),
+            w4: wn(4), w5: wn(5), w6: wn(6),
+        }
         return result;
-    };
+    }
 
     /** Traverse the dom and apply style */
     function applyStyle(domNode, styles) {
