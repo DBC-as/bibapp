7150a52b352af1ea5ad14e2f1e9c356f90e5600b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2013-01-05 16:09:23 +0100

    mobile meta

--- bibapp.js
+++ bibapp.js
@@ -1248,6 +1248,12 @@
                     ["head",
                         ["title", "BibApp"],
                         ["meta", {"http-equiv": "Content-Type", content: "text/html; charset=UTF-8"}],
+                        ["meta", {"http-equiv": "X-UA-Compatible", content: "IE=edge,chrome=1"}],
+                        ["meta", {"name": "HandheldFriendly", content: "true"}],
+                        ["meta", {"name": "viewport", content: "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0"}],
+                        ["meta", {"name": "format-detection", content: "telephone=no"}],
+                        ["meta", {"name": "apple-mobile-web-app-capable", content: "yes"}],
+                        ["meta", {"name": "apple-mobile-web-app-status-bar-style", content: "black"}],
                         ["link", {rel: "stylesheet", href: "/depend/font-awesome.css"}],
                         ["script", {src: "/depend/socket.io.min.js"}, ""],
                         ["script", {src: "/bibapp.js"}, ""]
