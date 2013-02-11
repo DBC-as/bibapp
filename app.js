ae04f5ebe22e0a71dac62e9e488fd9fdabfbc953
Rasmus Erik <github-rasmuserik@solsort.dk>
2013-02-11 22:23:26 +0100

    web service communication

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 require("qp")(global);
 
+var ws = {};
+
+ws.search = function(query, first, callback) {
+    qp.sys.read("http://opensearch.addi.dk/2.2/", {
+        "post": {"xml": qp.jsonml.toString(
+            ["soap:Envelope",
+                {   "xmlns:soap":"http://schemas.xmlsoap.org/soap/envelope/",
+                    xmlns:"http://oss.dbc.dk/ns/opensearch"},
+                ["soap:Body", ["searchRequest",
+                    ["query", query],
+                    ["agency", "100200"],
+                    ["profile", "test"],
+                    ["start", (first || 0) +1],
+                    ["callback", "callback"],
+                    ["stepValue", "10"]]]])}
+    }, function(err, data, opt) {
+        if(err) return callback(err);
+        callback(null, qp.jsonml.fromString(data), {});
+    });
+};
+
 function search(client) {
-    client.json(client);
+    console.log(client);
+    ws.search("query", 0, function(err, result) {
+        client.jsonml(JSON.stringify(result[0][2][2][2]));
+    });
 }
 
 function bibentry(client) {
