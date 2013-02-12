5d7701616da258dede4485d6b8a839e4257d8c86
Rasmus Erik <github-rasmuserik@solsort.dk>
2013-02-12 16:13:10 +0100

    opensearch

--- app.js
+++ app.js
@@ -1,71 +1,91 @@
 require("qp")(global);
 
-function directSearch(client) {
-    var query = client.route.args[0];
-    var first = Number(client.route.args[1]) || 0;
-    var openSearchUrl = "http://opensearch.addi.dk/2.2/";
-    var searchEnvelope = qp.jsonml.toString(["soap:Envelope", {
+// simplify opensearch-response-xml into json
+function openSearchResponseToJson(jsonml) {
+    jsonml = jsonml[2][2][2];
+    var result = {results: []};
+    for(var i = 2; i < jsonml.length; ++i) {
+        var current = jsonml[i];
+        var tagname = current[0];
+        if(tagname === "hitCount") {
+            result["hitCount"] = Number(current[2]);
+        } else if(tagname === "searchResult") {
+            var objects = [];
+            current = current[2];
+            for(var j = 2; j < current.length; ++j) {
+                if(current[j][0] === "object") {
+                    var object = [];
+                    for(var k = 2; k < current[j].length; ++k) {
+                        if(current[j][k][0] === "dkabm:record") {
+                            object = object.concat(current[j][k].slice(2));
+                        } else {
+                            object.push(current[j][k]);
+                        }
+                    }
+                    objects.push(object);
+                }
+            }
+            result.results.push(objects);
+        }
+    }
+    return result;
+}
+
+function openSearchEnvelope(query, first) {
+    return qp.jsonml.toString(["soap:Envelope", {
             "xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/",
             "xmlns": "http://oss.dbc.dk/ns/opensearch"},
         ["soap:Body", ["searchRequest",
             ["query", query],    ["start", first + 1],
             ["profile", "test"], ["agency", "100200"],
             ["stepValue", "10"]]]]);
+}
+
+function openSearchSimple(query, first, callback) {
+    var openSearchUrl = "http://opensearch.addi.dk/2.2/";
 
     // send the query
-    qp.sys.read(openSearchUrl, {post: {xml: searchEnvelope}}, handleResponse);
-    function handleResponse(err, data, opt) {
-        if(err) throw err;
+    qp.sys.read(openSearchUrl, {post: {xml: openSearchEnvelope(query, first)}}, handleResponse);
+    function handleResponse(err, data) {
+        if(err) return callback(err);
 
         // parse xml, and skip containing tags.
-        var jsonml = qp.jsonml.fromString(data)[0][2][2][2];
+        var jsonml = qp.jsonml.fromString(data)[0];
 
         // throw on error message
-        if(jsonml[0] === "error") throw qp.jsonml.toString(jsonml);
+        if(jsonml[2][2][2] === "error") return callback(qp.jsonml.toString(jsonml));
 
-        console.log(data);
-        // transform the xml into som slightly more friendly json
-        var result = {first: first, results: []};
-        for(var i = 2; i < jsonml.length; ++i) {
-            var current = jsonml[i];
-            var tagname = current[0];
-            if(tagname === "hitCount") {
-                result["hitCount"] = Number(current[2]);
-            } else if(tagname === "searchResult") {
-                var objects = [];
-                current = current[2];
-                for(var j = 2; j < current.length; ++j) {
-                    if(current[j][0] === "object") {
-                        var object = [];
-                        for(var k = 2; k < current[j].length; ++k) {
-                            if(current[j][k][0] === "dkabm:record") {
-                                object = object.concat(current[j][k].slice(2));
-                            } else if(current[j][k][0] === "dkabm:record") {
-                                object.push(current[j][k]);
-                            }
-                        }
-                        objects.push(object);
-                    }
-                }
-                result.results.push(objects);
-            }
-        }
-        client.json(result);
+        callback(null, openSearchResponseToJson(jsonml));
     }
 }
 
-function showSearchResults(client, results) {
+function clientOpenSearch(client) {
+    openSearchSimple(query, first, function(err, result) {
+        if(err) {
+            client.json({error: err});
+        } else {
+            client.json(result);
+        }
+    });
+}
+
+function showSearchResults(client, err, results) {
+    if(err) throw err;
+    client.json(results);
 }
 
 function search(client) {
+    var query = client.route.args[0];
+    var first = Number(client.route.args[1]) || 0;
+    var showFn = showSearchResults.bind(null, client);
+
     if(qp.platform.nodejs) {
-        directSearch(client);
+        openSearchSimple(query, first, showFn);
+
     } else if(qp.platform.html5) {
         qp.ui.showLoadingIndicator({title: "Søger..."});
-        qp.route.rpc("search", {args: client.args}, function(err, data) {
-            if(err) throw err;
-            showSearchResults(client, results);
-        });
+        qp.route.rpc("search", {args: [query, first]}, showFn);
+
     } else {
         throw "unknown platform"
     }
