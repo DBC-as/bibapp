cd2ad9f55c1fadbe3f0116e409c26023339536da
Rasmus Erik <github-rasmuserik@solsort.dk>
2013-02-12 15:39:06 +0100

    talking with open-search web service

--- app.js
+++ app.js
@@ -1,31 +1,74 @@
 require("qp")(global);
 
-var ws = {};
+function directSearch(client) {
+    var query = client.route.args[0];
+    var first = Number(client.route.args[1]) || 0;
+    var openSearchUrl = "http://opensearch.addi.dk/2.2/";
+    var searchEnvelope = qp.jsonml.toString(["soap:Envelope", {
+            "xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/",
+            "xmlns": "http://oss.dbc.dk/ns/opensearch"},
+        ["soap:Body", ["searchRequest",
+            ["query", query],    ["start", first + 1],
+            ["profile", "test"], ["agency", "100200"],
+            ["stepValue", "10"]]]]);
 
-ws.search = function(query, first, callback) {
-    qp.sys.read("http://opensearch.addi.dk/2.2/", {
-        "post": {"xml": qp.jsonml.toString(
-            ["soap:Envelope",
-                {   "xmlns:soap":"http://schemas.xmlsoap.org/soap/envelope/",
-                    xmlns:"http://oss.dbc.dk/ns/opensearch"},
-                ["soap:Body", ["searchRequest",
-                    ["query", query],
-                    ["agency", "100200"],
-                    ["profile", "test"],
-                    ["start", (first || 0) +1],
-                    ["callback", "callback"],
-                    ["stepValue", "10"]]]])}
-    }, function(err, data, opt) {
-        if(err) return callback(err);
-        callback(null, qp.jsonml.fromString(data), {});
-    });
-};
+    // send the query
+    qp.sys.read(openSearchUrl, {post: {xml: searchEnvelope}}, handleResponse);
+    function handleResponse(err, data, opt) {
+        if(err) throw err;
+
+        // parse xml, and skip containing tags.
+        var jsonml = qp.jsonml.fromString(data)[0][2][2][2];
+
+        // throw on error message
+        if(jsonml[0] === "error") throw qp.jsonml.toString(jsonml);
+
+        console.log(data);
+        // transform the xml into som slightly more friendly json
+        var result = {first: first, results: []};
+        for(var i = 2; i < jsonml.length; ++i) {
+            var current = jsonml[i];
+            var tagname = current[0];
+            if(tagname === "hitCount") {
+                result["hitCount"] = Number(current[2]);
+            } else if(tagname === "searchResult") {
+                var objects = [];
+                current = current[2];
+                for(var j = 2; j < current.length; ++j) {
+                    if(current[j][0] === "object") {
+                        var object = [];
+                        for(var k = 2; k < current[j].length; ++k) {
+                            if(current[j][k][0] === "dkabm:record") {
+                                object = object.concat(current[j][k].slice(2));
+                            } else if(current[j][k][0] === "dkabm:record") {
+                                object.push(current[j][k]);
+                            }
+                        }
+                        objects.push(object);
+                    }
+                }
+                result.results.push(objects);
+            }
+        }
+        client.json(result);
+    }
+}
+
+function showSearchResults(client, results) {
+}
 
 function search(client) {
-    console.log(client);
-    ws.search("query", 0, function(err, result) {
-        client.jsonml(JSON.stringify(result[0][2][2][2]));
-    });
+    if(qp.platform.nodejs) {
+        directSearch(client);
+    } else if(qp.platform.html5) {
+        qp.ui.showLoadingIndicator({title: "Søger..."});
+        qp.route.rpc("search", {args: client.args}, function(err, data) {
+            if(err) throw err;
+            showSearchResults(client, results);
+        });
+    } else {
+        throw "unknown platform"
+    }
 }
 
 function bibentry(client) {
